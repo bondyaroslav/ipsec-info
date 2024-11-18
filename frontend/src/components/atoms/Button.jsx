bd5b7abd047e05db73e2adffca62ab8c0ca3ff04
bondyaroslav <bondy2526@gmail.com>
2024-11-19 01:26:55 +0300

    Add Button, Title, Notification, CodeBlock, InfoCard, Main  components

---
+++ frontend/src/components/atoms/Button.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Button from '@mui/material/Button'
+import { styled } from '@mui/material'
+
+const StyledButton = styled(Button)({
+    height: '100%',
+    color: '#787878',
+    backgroundColor: 'transparent',
+    boxShadow: 'none',
+    '&:hover': {
+        backgroundColor: 'transparent',
+        color: '#9e9e9e',
+        boxShadow: 'none',
+    },
+})
+
+export const CustomButton = ({ variant, color, name }) => {
+    return (
+        <StyledButton variant={variant} color={color}>
+            {name}
+        </StyledButton>
+    )
+}
