93549297677fba275971e0d93e65377fb0873dc9
bondyaroslav <bondy2526@gmail.com>
2024-11-22 03:39:32 +0300

    Change origin for cors

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors({
-    origin: ['https://ipsec-info-as1lnx4rq-bondyaroslavs-projects.vercel.app', 'http://localhost:5173'],
+    origin: ['https://ipsec-info.onrender.com', 'http://localhost:5173'],
     credentials: true,
 }));
 app.use(bodyParser.json());
